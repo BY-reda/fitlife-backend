4e251d4f199f05658f6cde39c4685948f4243ac5
reda <workfor2002@gmail.com>
2025-08-01 17:04:32 +0100

    change the db adding mongodb local and atlas condition

--- config/db.js
+++ config/db.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
+  try {
+    const mongoURI =
+      process.env.NODE_ENV === 'production'
+        ? process.env.MONGO_URI_ATLAS
+        : process.env.MONGO_URI_LOCAL;
+
+    const conn = await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
